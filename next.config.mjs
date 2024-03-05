d230852aae92d3a6105840026b069d8d22f2e2b5
Carolina Di Matteo <cdimatteo@fi.uba.ar>
2024-03-05 00:30:42 -0300

    remove nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    basePath: "/introds-web",
 };
 
 export default nextConfig;
