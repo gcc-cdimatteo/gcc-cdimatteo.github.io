363c051757a64e0b5f7f6b98faa3a5c9fd87a1e0
Carolina Di Matteo <cdimatteo@fi.uba.ar>
2024-03-10 22:21:51 -0300

    corrijo un gt

--- app/cursada/page.tsx
+++ app/cursada/page.tsx
@@ -13,7 +13,7 @@ export default function Page() {
       <ListItem icon={<CheckBox sx={{ fontSize: 16, color: '#006EAF' }} />} text={<p>Aprobar el Trabajo Práctico Grupal</p>} />
       <ListItem icon={<CheckBox sx={{ fontSize: 16, color: '#006EAF' }} />} text={<p>Tener más del 75% de la asistencia</p>} />
       <p>La materia se <b>promociona</b> con <u>más de 7 (siete) puntos de promedio</u>. Es decir:</p>
-      <ListItem icon={<Info sx={{ fontSize: 16, color: '#006EAF' }} />} text={<p>([Primer Parcialito]+[Segundo Parcialito]+[Parcial]+[TP Grupal]) / 4 > 7 → <b>PROMOCIONA</b> (NO RINDE FINAL)</p>} />
+      <ListItem icon={<Info sx={{ fontSize: 16, color: '#006EAF' }} />} text={<p>([Primer Parcialito]+[Segundo Parcialito]+[Parcial]+[TP Grupal]) / 4 &gt; 7 → <b>PROMOCIONA</b> (NO RINDE FINAL)</p>} />
       <ListItem icon={<Info sx={{ fontSize: 16, color: '#006EAF' }} />} text={<p>([Primer Parcialito] + [Segundo Parcialito] + [Parcial] + [TP Grupal]) / 4 ≤ 7 → <b>RINDE FINAL</b></p>} />
     </Typography>
   </>;
